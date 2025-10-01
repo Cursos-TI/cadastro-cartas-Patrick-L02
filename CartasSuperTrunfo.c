1435a70f014418eb15ec9ff46a9055ad76e90e65
Patrick-L02 <202508832275@alunos.estacio.br>
2025-10-01 05:34:17 +0000

    adicionando a primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,40 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  //variaveis da carta 1
+char estado1[5];
+char codigo1[5];
+char cidade1[10];
+int populacao1;
+float area1;
+float pib1;
+int turistico1;
+//variaveis da carta 2
+char estado2;
+char codigo2[5];
+char cidade2[10];
+int populacao2;
+float area2;
+float pib2;
+int turistico2;
   // Área para entrada de dados
+printf("Vamos criar duas cartas\nPrimeira carta\nDigite o estado da primeira carta: ");
+scanf("%s", estado1);
+printf("Agora digite o codigo da carta: ");
+scanf("%s", codigo1);
+printf("Agora digite a cidade: ");
+scanf("%s", cidade1);
+printf("Agora digite a população: ");
+scanf("%d", &populacao1);
+printf("Agora digite a area: ");
+scanf("%f", &area1);
+printf("Agora digite o pib: ");
+scanf("%f", &pib1);
+printf("Agora digite a quantidade de pontos turísticos: ");
+scanf("%d", &turistico1);
+//secunda carta
 
   // Área para exibição dos dados da cidade
-
+printf("Primeira carta:\nEstado:%s\nCodigo:%s\nCidade:%s\n", estado1, codigo1, cidade1);
 return 0;
 } 
